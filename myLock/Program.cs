4f69e2fa44a85b662b3cff5e865644de8cfcc65d
Sven Rütz <ruetz@itisim.com>
2016-02-10 17:42:14 +0100

    added ability to "init" and "deinit" scheduled tasks from commandline: admin privs needed

--- myLock/Program.cs
+++ myLock/Program.cs
@@ -7,25 +7,33 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using Microsoft.Win32.TaskScheduler;
+using System.Reflection;
 
 namespace myLock
 {
     class Program
     {
         private static string dbFile = Environment.CurrentDirectory + "\\times.db";
+        private static string taskPrefix = "__myLock__";
+
         static void Main(string[] args)
         {
             string direction = string.Empty;
             bool query = false;
             bool raw = false;
+            bool init = false;
+            bool deInit = false;
             string inject=string.Empty;
 
             var configuration = CommandLineParserConfigurator
                 .Create()
                 .WithPositional(d => direction = d)
-                .WithSwitch("q", () => query = true)
-                .WithSwitch("r", () => raw = true)
-                .WithNamed("i", I => inject = I)
+                .WithSwitch("query", () => query = true)
+                .WithSwitch("raw", () => raw = true)
+                .WithSwitch("init", () => init = true)
+                .WithSwitch("deinit", () => deInit = true)
+                .WithNamed("inject", I => inject = I)
                 .BuildConfiguration();
             var parser = new CommandLineParser(configuration);
 
@@ -33,10 +41,52 @@ namespace myLock
 
             if (!parseResult.Succeeded)
             {
+                Console.WriteLine("wrong arguments");
                 Environment.Exit(-1);
             }
             else
             {
+                if (init)
+                {
+                    using (TaskService ts = new TaskService())
+                    {
+                        try
+                        {
+                            DefineTask(ts, "myLock lock screen", "lock_pc", TaskSessionStateChangeType.SessionLock);
+                            DefineTask(ts, "myLock unlock screen", "unlock_pc", TaskSessionStateChangeType.SessionUnlock);
+                            DefineTask(ts, "myLock login to pc", "login_pc", TaskSessionStateChangeType.ConsoleConnect);
+                            DefineTask(ts, "myLock logout from pc", "logout_pc", TaskSessionStateChangeType.ConsoleDisconnect);
+                            Console.WriteLine("Initialization complete.");
+                            Environment.Exit(0);
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            Console.Error.WriteLine("Access denied, please use an elevated prompt.");
+                            Environment.Exit(-1);
+                        }
+                    }
+                }
+                if (deInit)
+                {
+                    using (TaskService ts = new TaskService())
+                    {
+                        try
+                        {
+                            //Remove tasks
+                            foreach (var t in ts.RootFolder.AllTasks)
+                                if (t.Name.StartsWith(taskPrefix))
+                                    ts.RootFolder.DeleteTask(t.Name);
+
+                            Console.WriteLine("Deinitialization complete.");
+                            Environment.Exit(0);
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            Console.Error.WriteLine("Access denied, please use an elevated prompt.");
+                            Environment.Exit(-1);
+                        }
+                    }
+                }
                 if (!string.IsNullOrEmpty(inject))
                 {
                     TimeStampCollection col;
@@ -60,7 +110,9 @@ namespace myLock
                     col.TimeStamps.Sort();
                     EncryptAndSerialize<TimeStampCollection>(dbFile, col, GetKey());
 
-                    Environment.Exit(1);
+                    Console.WriteLine("Injection successfull.");
+                    Console.WriteLine("Values were: {0}", inject);
+                    Environment.Exit(0);
                 }
 
                 if (raw)
@@ -69,6 +121,7 @@ namespace myLock
                     foreach (var t in col.TimeStamps)
                         Console.WriteLine("{0} {1} {2}", t.Stamp, t.User, t.Direction);
 
+                    Console.WriteLine("EOF");
                     Environment.Exit(0);
                 }
 
@@ -79,6 +132,29 @@ namespace myLock
             }
         }
 
+        private static void DefineTask(TaskService ts, string description, string taskName, TaskSessionStateChangeType sessionType)
+        {
+            string executable = string.Format("{0}\\mylock.exe", AssemblyDirectory);
+            TaskDefinition td = ts.NewTask();
+            td.Principal.RunLevel = TaskRunLevel.LUA;
+            td.RegistrationInfo.Description = description;
+            td.Triggers.Add(new SessionStateChangeTrigger(sessionType));
+            td.Actions.Add(new ExecAction(executable,
+                (sessionType == TaskSessionStateChangeType.SessionLock || 
+                sessionType == TaskSessionStateChangeType.ConsoleDisconnect) ? "lock" : "", AssemblyDirectory));
+            ts.RootFolder.RegisterTaskDefinition(taskPrefix + taskName, td);
+        }
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
         private static void QueryWorkingTimes()
         {
 #if !DEBUG
@@ -180,7 +256,17 @@ namespace myLock
 
         private static void PrintTime(DateTime dtIn, DateTime dtOut)
         {
-            Console.WriteLine("{0}\t {1} in and {2} out. {3:hh\\:mm} h of work", dtIn.Date, dtIn.TimeOfDay, dtOut.TimeOfDay, (dtOut - dtIn));
+            try {
+                if (dtOut == DateTime.MinValue)
+                    Console.WriteLine("{0:yyyy-MM-dd ddd}\t {1:HH\\:mm} in and till now ({2:HH\\:mm}) {3:hh\\:mm} h of work", dtIn.Date, dtIn, DateTime.Now, (DateTime.Now - dtIn));
+                else
+                    Console.WriteLine("{0:yyyy-MM-dd ddd}\t {1:HH\\:mm} in and {2:HH\\:mm} out. {3:hh\\:mm} h of work", dtIn.Date, dtIn, dtOut, (dtOut - dtIn));
+            }
+            catch(FormatException ex)
+            {
+                Console.Error.WriteLine("dtIn {0}", dtIn);
+                Console.Error.WriteLine("dtOut {0}", dtOut);
+            }
         }
 
         private static void LogTimeStamp(string direction)
@@ -216,6 +302,7 @@ namespace myLock
 
             col.TimeStamps.Add(stamp);
             EncryptAndSerialize<TimeStampCollection>(dbFile, col, sMyKey);
+            Console.WriteLine("Saved: {0}|", stamp.Stamp, stamp.Direction);
         }
 
         private static string GetKey()
@@ -237,12 +324,19 @@ namespace myLock
             byte[] k = Encoding.ASCII.GetBytes(encryptionKey.Substring(0,length));
             byte[] iV = Encoding.ASCII.GetBytes(encryptionKey.Substring(length));
             var e = key.CreateEncryptor(k, iV);
-            using (var fs = File.Open(filename, FileMode.Create))
-            {
-                using (var cs = new CryptoStream(fs, e, CryptoStreamMode.Write))
+            try {
+                using (var fs = File.Open(filename, FileMode.Create))
                 {
-                    (new XmlSerializer(typeof(T))).Serialize(cs, obj);
+                    using (var cs = new CryptoStream(fs, e, CryptoStreamMode.Write))
+                    {
+                        (new XmlSerializer(typeof(T))).Serialize(cs, obj);
+                    }
                 }
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                Environment.Exit(-1);
             }
         }
 
@@ -253,9 +347,18 @@ namespace myLock
             byte[] k = Encoding.ASCII.GetBytes(encryptionKey.Substring(0, length));
             byte[] iV = Encoding.ASCII.GetBytes(encryptionKey.Substring(length));
             var d = key.CreateDecryptor(k, iV);
-            using (var fs = File.Open(filename, FileMode.Open))
-            using (var cs = new CryptoStream(fs, d, CryptoStreamMode.Read))
-                return (T)(new XmlSerializer(typeof(T))).Deserialize(cs);
+            try
+            {
+                using (var fs = File.Open(filename, FileMode.Open))
+                using (var cs = new CryptoStream(fs, d, CryptoStreamMode.Read))
+                    return (T)(new XmlSerializer(typeof(T))).Deserialize(cs);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine("{0} could not be found", filename);
+                Environment.Exit(-1);
+                return default(T);
+            }
         }
     }
 }
